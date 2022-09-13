e80ad1b773a9f3e4502c62ffe1dac7a4941e1b0d
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-09-13 00:04:08 -0400

    Added readme.md

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ if __name__ == '__main__':
     bot_token = ''
     api_token = ''
 
+    # TODO: Move this into a class all of its own
+    # TODO: Make functionality that if secret.json doesn't exist, a new file can be created taking in the needed API tokens
+    # TODO: Create a nicer message template
+    # TODO: Implement search for gathering locations of materials
+
     if os.path.exists('secret.json'):
         with open('secret.json', 'r') as f:
             lines = json.load(f)
---
+++ readme.md
@@ -0,0 +1,8 @@
+Meet Craftingway! This helpful Discord bot is for any and all crafters
+and gatherers in FFXIV! Craftingway is still learning his way around
+the many crafts of Eorzea, but so far he can show you what items are
+required, and how much of that item.
+
+In time, Craftingway will be able to give tons of more information, such
+as gathering spots, nested crafts, and so much more!
+
