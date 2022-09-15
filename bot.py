4c0685db2a35063b23d7fec7096d6c186a932a85
Tyler Ditlow <59483267+TylerD94@users.noreply.github.com>
2022-09-15 18:30:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,12 +13,34 @@ async def on_ready():
     print(f"{bot.user} is online!")
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    msg = Messages(message, api, users)
-    await msg.new_message()
+@bot.command(name='craft', description='Returns crafting recipe')
+async def craft(ctx, *args):
+    msg = Messages(ctx, *args, api, users)
+    await msg.lookup_item()
+
+
+@bot.command(name='additem', description="Adds item to user's saved-items list")
+async def add_item(ctx, *args):
+    msg = Messages(ctx, *args, api, users)
+    await msg.add_item()
+
+
+@bot.command(name='removeitem', description="Removes item from user's saved-items list")
+async def remove_item(ctx, *args):
+    msg = Messages(ctx, *args, api, users)
+    await msg.remove_item()
+
+
+@bot.command(name='saveditems', description="Sends a message with the user's saved-items list")
+async def saved_items(ctx):
+    msg = Messages(ctx, api, users)
+    await msg.saved_items()
+
+
+@bot.command(name='help', description='Sends a help message')
+async def help(ctx):
+    msg = Messages(ctx, api, users)
+    await msg.help()
 
 
 def start(bot_tkn, api_tkn):
